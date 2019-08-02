0c9343c227d75a7c7eb31e24aaf17dfafc4bc6df
Dennis Effa Amponsah <kamponsah.amponsah@gmail.com>
2019-08-02 12:40:27 +0100

    Setup

--- src/main/java/glblcd/Main.java
+++ src/main/java/glblcd/Main.java
@@ -1,9 +1,19 @@
 package glblcd;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+
 public class Main {
 
+    public static void main(String[] args) throws IOException {
+        var file = Files.readAllBytes(Paths.get("src/main/resources/cake.json"));
 
-    public static void main(String[] args) {
-
+//        var map = new ObjectMapper();
+        System.out.println(file);
     }
 }

14de880f33b3d204d6f3f13747e9a75efac5185f
Dennis Effa Amponsah <kamponsah.amponsah@gmail.com>
2019-08-02 15:19:21 +0100

    Data successfully fetched from endpoint

--- src/main/java/glblcd/Main.java
+++ src/main/java/glblcd/Main.java
@@ -5,6 +5,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.lang.model.type.ReferenceType;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
@@ -12,19 +18,29 @@ import java.util.Map;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        var fileContent = Files.readAllBytes(Paths.get("src/main/resources/cake.json"));
-        var json = new String(fileContent);
+    public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
+        String location = "https://facebook.github.io/react-native/movies.json";
+        var client = HttpClient.newBuilder().build();
+        URI url;
+
+        url = new URI(location);
+
+        var request = HttpRequest.newBuilder(url).build();
+        var res = client.send(request, HttpResponse.BodyHandlers.ofString(Charset.defaultCharset()));
+
+        System.out.println(res.body());
+
+        Main m = new Main();
+        m.run();
+
+    }
 
 
-        System.out.println(fileContent);
 
-        var map = new ObjectMapper();
 
-        var type = new TypeReference<HashMap<String, Object>>(){};
-
-        Map<String, Object> productJson = map.readValue(json, type);
-                System.out.println(productJson);
-
+    private void run() throws IOException {
+        String json = null;
+            var contents = Files.readAllBytes(Paths.get("src/main/resources/cake.json"));
+            json = new String(contents);
     }
 }

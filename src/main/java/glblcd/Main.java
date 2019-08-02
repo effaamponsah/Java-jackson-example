dd8661b97a48c9234d0404f35b7dc7b04ff9aa79
Dennis Effa Amponsah <kamponsah.amponsah@gmail.com>
2019-08-02 14:37:50 +0100

    Done displaying data from json file

--- src/main/java/glblcd/Main.java
+++ src/main/java/glblcd/Main.java
@@ -3,17 +3,28 @@ package glblcd;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import javax.lang.model.type.ReferenceType;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        var file = Files.readAllBytes(Paths.get("src/main/resources/cake.json"));
+        var fileContent = Files.readAllBytes(Paths.get("src/main/resources/cake.json"));
+        var json = new String(fileContent);
 
-//        var map = new ObjectMapper();
-        System.out.println(file);
+
+        System.out.println(fileContent);
+
+        var map = new ObjectMapper();
+
+        var type = new TypeReference<HashMap<String, Object>>(){};
+
+        Map<String, Object> productJson = map.readValue(json, type);
+                System.out.println(productJson);
+
     }
 }
